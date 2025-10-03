80217b2cb5592dd7d5480fb11ffad11beaf9def8
wannachaikung <wannachai200412@gmail.com>
2025-10-03 09:58:02 +0700

    feat: Secure Supabase keys with environment variables

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,8 @@
-
+require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://zblvxpznzmenrxiqgcas.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpibHZ4cHpuem1lbnJ4aXFnY2FzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4MTMyOTEsImV4cCI6MjA3NDM4OTI5MX0.sDNMTbDFIthNNortgvj1ZnG4fSPPcc1CR4EiFDVQmOg';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
